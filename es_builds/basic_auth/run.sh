52c7ebb52575cfd7204e063ca2bc13ffa42133ee
SgtMarmite <d.novotny31415@gmail.com>
2023-11-06 13:59:52 +0100

    add scheme support

---
+++ es_builds/basic_auth/run.sh
@@ -0,0 +1,1 @@
+docker run --name elasticsearch-basic -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "ELASTIC_PASSWORD=root" -e "xpack.security.http.ssl.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:8.10.4
