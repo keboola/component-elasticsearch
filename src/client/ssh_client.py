aa4333188cac7aeb24e4cecfe630690c3dcfa9ed
SgtMarmite <d.novotny31415@gmail.com>
2023-11-07 10:10:29 +0100

    rework ssh connection

--- src/client/ssh_client.py
+++ src/client/ssh_client.py
@@ -29,14 +29,14 @@ class SSHClient:
             pkey=private_key
         )
 
-    def setup_tunnel(self, remote_host, remote_port: int, local_port: int):
+    def setup_tunnel(self, remote_host, remote_port: int):
         if not self.ssh_client:
             raise RuntimeError("SSH connection is not established")
 
         transport = self.ssh_client.get_transport()
 
-        logging.info(f"Tunel local_port: {local_port}, remote_host: {remote_host}, remote port: {remote_port}")
-        transport.request_port_forward(("", local_port), remote_host, remote_port)
+        logging.info(f"Tunel remote_host: {remote_host}, remote port: {remote_port}")
+        transport.request_port_forward(remote_host, remote_port)
 
     def close(self):
         if self.ssh_client:
