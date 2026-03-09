0d2ae55362959659068fc00d48ea78bddfca26a1
Josef Kudera <46950237+kudj@users.noreply.github.com>
2026-03-09 15:22:28 +0100

    fix order of columns

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,13 +1,20 @@
 import unittest
 
 from keboola.datadirtest import DataDirTester
+from keboola.datadirtest.datadirtest import TestDataDir
 from freezegun import freeze_time
+
+
+class TestDataDirWithState(TestDataDir):
+    def _override_input_state(self, input_state: dict):
+        if input_state is not None:
+            super()._override_input_state(input_state)
 
 
 class TestFunctional(unittest.TestCase):
     @freeze_time("2024-01-01")
     def test_functional(self):
-        functional_tests = DataDirTester()
+        functional_tests = DataDirTester(test_data_dir_class=TestDataDirWithState)
         functional_tests.run()
 
 
