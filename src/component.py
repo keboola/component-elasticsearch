680cf2f5022c96ae7f1e478db729cdd046ff9bfa
Josef Kudera <46950237+kudj@users.noreply.github.com>
2026-03-10 11:41:54 +0100

    add pydantic based configuration.py

--- src/component.py
+++ src/component.py
@@ -1,8 +1,9 @@
 import json
 import logging
-import uuid
-import os
 import shutil
+import os
+import uuid
+
 import dateparser
 import pytz
 
@@ -11,53 +12,16 @@ from keboola.component.exceptions import UserException
 from keboola.csvwriter import ElasticDictWriter
 
 from client.es_client import ElasticsearchClient
-from legacy_client.legacy_es_client import LegacyClient
 from client.ssh_utils import SomeSSHException, get_private_key
-from sshtunnel import SSHTunnelForwarder, BaseSSHTunnelForwarderError
-
-# configuration variables
-KEY_GROUP_DB = "db"
-KEY_DB_HOSTNAME = "hostname"
-KEY_DB_PORT = "port"
-KEY_QUERY = "request_body"  # this is named like this for backwards compatibility
-KEY_INDEX_NAME = "index_name"
-KEY_STORAGE_TABLE = "storage_table"
-KEY_PRIMARY_KEYS = "primary_keys"
-KEY_INCREMENTAL = "incremental"
-KEY_GROUP_AUTH = "authentication"
-KEY_AUTH_TYPE = "auth_type"
-KEY_USERNAME = "username"
-KEY_PASSWORD = "#password"
-KEY_API_KEY_ID = "api_key_id"
-KEY_API_KEY = "#api_key"
-KEY_BEARER = "#bearer"
-KEY_SCHEME = "scheme"
-
-KEY_GROUP_DATE = "date"
-KEY_DATE_APPEND = "append_date"
-KEY_DATE_FORMAT = "format"
-KEY_DATE_SHIFT = "shift"
-KEY_DATE_TZ = "time_zone"
-DATE_PLACEHOLDER = "{{date}}"
-DEFAULT_DATE = "yesterday"
-DEFAULT_DATE_FORMAT = "%Y-%m-%d"
-DEFAULT_TZ = "UTC"
-
-KEY_SSH = "ssh_options"
-KEY_USE_SSH = "enabled"
-KEY_SSH_KEYS = "keys"
-KEY_SSH_PRIVATE_KEY = "#private"
-KEY_SSH_USERNAME = "user"
-KEY_SSH_TUNNEL_HOST = "sshHost"
-KEY_SSH_TUNNEL_PORT = "sshPort"
+from configuration import AuthType, Configuration
+from legacy_client.legacy_es_client import LegacyClient
+from sshtunnel import BaseSSHTunnelForwarderError, SSHTunnelForwarder
 
 LOCAL_BIND_ADDRESS = "127.0.0.1"
 
-KEY_LEGACY_SSH = "ssh"
-
-REQUIRED_PARAMETERS = [KEY_GROUP_DB]
-
 RSA_HEADER = "-----BEGIN RSA PRIVATE KEY-----"
+
+DATE_PLACEHOLDER = "{{date}}"
 
 
 class Component(ComponentBase):
@@ -65,56 +29,56 @@ class Component(ComponentBase):
         super().__init__()
 
     def run(self):
-        self.validate_configuration_parameters(REQUIRED_PARAMETERS)
+        self.validate_configuration_parameters(["db"])
         params = self.configuration.parameters
 
-        if params.get(KEY_LEGACY_SSH):
+        config = Configuration(**params)
+
+        if config.ssh is not None:
             self.run_legacy_client()
-        else:
-            out_table_name = params.get(KEY_STORAGE_TABLE, False)
-            if not out_table_name:
-                out_table_name = "ex-elasticsearch-result"
-                logging.info(f"Using default output table name: {out_table_name}")
+            return
 
-            user_defined_pk = params.get(KEY_PRIMARY_KEYS, [])
-            incremental = params.get(KEY_INCREMENTAL, False)
+        out_table_name = config.storage_table
+        logging.info(f"Using output table name: {out_table_name}")
 
-            index_name, query = self.parse_index_parameters(params)
-            statefile = self.get_state_file()
+        index_name, query = self.parse_index_parameters(config)
+        statefile = self.get_state_file()
 
-            ssh_options = params.get(KEY_SSH)
-            use_ssh_tunnel = False
-            # fix eternal KBC issue
-            if ssh_options is not None and not isinstance(ssh_options, list):
-                if ssh_options.get(KEY_USE_SSH, False):
-                    self._create_and_start_ssh_tunnel(params)
-                    use_ssh_tunnel = True
+        use_ssh_tunnel = False
+        ssh_opts = config.ssh_options
+        # Guard against KBC returning ssh_options as an empty list instead of null
+        if ssh_opts is not None and ssh_opts.enabled:
+            self._create_and_start_ssh_tunnel(config)
+            use_ssh_tunnel = True
 
-            client = self.get_client(params, hostname_override=LOCAL_BIND_ADDRESS if use_ssh_tunnel else None)
+        hostname_override = LOCAL_BIND_ADDRESS if use_ssh_tunnel else None
+        client = self.get_client(config, hostname_override=hostname_override)
 
-            temp_folder = os.path.join(self.data_folder_path, "temp")
-            os.makedirs(temp_folder, exist_ok=True)
+        temp_folder = os.path.join(self.data_folder_path, "temp")
+        os.makedirs(temp_folder, exist_ok=True)
 
-            columns = statefile.get(out_table_name, [])
-            out_table = self.create_out_table_definition(
-                out_table_name, primary_key=user_defined_pk, incremental=incremental
-            )
+        columns = statefile.get(out_table_name, [])
+        out_table = self.create_out_table_definition(
+            out_table_name,
+            primary_key=config.primary_keys,
+            incremental=config.incremental,
+        )
 
-            try:
-                with ElasticDictWriter(out_table.full_path, columns) as wr:
-                    for result in client.extract_data(index_name, query):
-                        wr.writerow(result)
-                    wr.writeheader()
-            except Exception as e:
-                raise UserException(f"Error occured while extracting data from Elasticsearch: {e}")
-            finally:
-                if hasattr(self, "ssh_server") and self.ssh_server.is_active:
-                    self.ssh_server.stop()
+        try:
+            with ElasticDictWriter(out_table.full_path, columns) as wr:
+                for result in client.extract_data(index_name, query):
+                    wr.writerow(result)
+                wr.writeheader()
+        except Exception as e:
+            raise UserException(f"Error occured while extracting data from Elasticsearch: {e}")
+        finally:
+            if hasattr(self, "ssh_server") and self.ssh_server.is_active:
+                self.ssh_server.stop()
 
-            self.write_manifest(out_table)
-            statefile[out_table_name] = wr.fieldnames
-            self.write_state_file(statefile)
-            self.cleanup(temp_folder)
+        self.write_manifest(out_table)
+        statefile[out_table_name] = wr.fieldnames
+        self.write_state_file(statefile)
+        self.cleanup(temp_folder)
 
     @staticmethod
     def run_legacy_client() -> None:
@@ -125,45 +89,33 @@ class Component(ComponentBase):
     def cleanup(temp_folder: str):
         shutil.rmtree(temp_folder)
 
-    def get_client(self, params: dict, hostname_override: str = None) -> ElasticsearchClient:
-        auth_params = params.get(KEY_GROUP_AUTH)
-        if not auth_params:
-            return self.get_client_legacy(params)
+    def get_client(self, config: Configuration, hostname_override: str = None) -> ElasticsearchClient:
+        auth = config.authentication
+        if auth is None:
+            return self.get_client_legacy(config)
 
-        db_params = params.get(KEY_GROUP_DB)
-        db_hostname = hostname_override if hostname_override else db_params.get(KEY_DB_HOSTNAME)
-        db_port = db_params.get(KEY_DB_PORT)
-        scheme = params.get(KEY_SCHEME, "http")
-
-        auth_type = auth_params.get(KEY_AUTH_TYPE, False)
-        if auth_type not in ["basic", "api_key", "bearer", "no_auth"]:
-            raise UserException(f"Invalid auth_type: {auth_type}")
+        db = config.db
+        db_hostname = hostname_override if hostname_override else db.hostname
+        db_port = db.port
+        scheme = config.scheme
 
         setup = {"host": db_hostname, "port": db_port, "scheme": scheme}
 
-        logging.info(f"The component will use {auth_type} type authorization.")
+        logging.info(f"The component will use {auth.auth_type} type authorization.")
 
-        if auth_type == "basic":
-            username = auth_params.get(KEY_USERNAME)
-            password = auth_params.get(KEY_PASSWORD)
+        if auth.auth_type == AuthType.basic:
+            http_auth = (auth.username, auth.password)
+            client = ElasticsearchClient([setup], scheme, http_auth=http_auth)
 
-            if not (username and password):
-                raise UserException("You must specify both username and password for basic type authorization")
+        elif auth.auth_type == AuthType.api_key:
+            api_key_tuple = (auth.api_key_id, auth.api_key)
+            client = ElasticsearchClient([setup], scheme, api_key=api_key_tuple)
 
-            auth = (username, password)
-            client = ElasticsearchClient([setup], scheme, http_auth=auth)
-
-        elif auth_type == "api_key":
-            api_key_id = auth_params.get(KEY_API_KEY_ID)
-            api_key = auth_params.get(KEY_API_KEY)
-            api_key = (api_key_id, api_key)
-            client = ElasticsearchClient([setup], scheme, api_key=api_key)
-
-        elif auth_type == "no_auth":
+        elif auth.auth_type == AuthType.no_auth:
             client = ElasticsearchClient([setup], scheme)
 
         else:
-            raise UserException(f"Unsupported auth_type: {auth_type}")
+            raise UserException(f"Unsupported auth_type: {auth.auth_type}")
 
         try:
             p = client.ping(error_trace=True)
@@ -175,30 +127,23 @@ class Component(ComponentBase):
         return client
 
     @staticmethod
-    def get_client_legacy(params) -> ElasticsearchClient:
-        db_params = params.get(KEY_GROUP_DB)
-        db_hostname = db_params.get(KEY_DB_HOSTNAME)
-        db_port = db_params.get(KEY_DB_PORT)
+    def get_client_legacy(config: Configuration) -> ElasticsearchClient:
+        db = config.db
+        setup = {"host": db.hostname, "port": db.port, "scheme": "http"}
+        return ElasticsearchClient([setup])
 
-        setup = {"host": db_hostname, "port": db_port, "scheme": "http"}
-        client = ElasticsearchClient([setup])
-
-        return client
-
-    def parse_index_parameters(self, params):
-        index = params.get(KEY_INDEX_NAME, "")
-        date_config = params.get(KEY_GROUP_DATE, {})
-        query = self._parse_query(params)
+    def parse_index_parameters(self, config: Configuration):
+        index = config.index_name
+        query = self._parse_query(config.request_body)
 
         if DATE_PLACEHOLDER in index:
-            index = self._replace_date_placeholder(index, date_config)
+            index = self._replace_date_placeholder(index, config)
 
         return index, query
 
     @staticmethod
-    def _parse_query(params):
-        _query = params.get(KEY_QUERY, "{}").strip()
-        query_string = _query if _query != "" else "{}"
+    def _parse_query(request_body: str) -> dict:
+        query_string = request_body.strip() or "{}"
 
         try:
             logging.info(f"Using query: {query_string}")
@@ -206,15 +151,16 @@ class Component(ComponentBase):
         except ValueError:
             raise UserException("Could not parse request body string to JSON.")
 
-    def _replace_date_placeholder(self, index, date_config):
-        _date = dateparser.parse(date_config.get(KEY_DATE_SHIFT, DEFAULT_DATE))
+    def _replace_date_placeholder(self, index: str, config: Configuration) -> str:
+        date_cfg = config.date
+        _date = dateparser.parse(date_cfg.shift)
         if _date is None:
-            raise UserException(f"Could not parse value {date_config[KEY_DATE_SHIFT]} to date.")
+            raise UserException(f"Could not parse value {date_cfg.shift} to date.")
 
         _date = _date.replace(tzinfo=pytz.UTC)
-        _tz = self._validate_timezone(date_config.get(KEY_DATE_TZ, DEFAULT_TZ))
+        _tz = self._validate_timezone(date_cfg.time_zone)
         _date_tz = pytz.timezone(_tz).normalize(_date)
-        _date_formatted = _date_tz.strftime(date_config.get(KEY_DATE_FORMAT, DEFAULT_DATE_FORMAT))
+        _date_formatted = _date_tz.strftime(date_cfg.format)
 
         logging.info(
             f"Replaced date placeholder with value {_date_formatted}. "
@@ -223,7 +169,7 @@ class Component(ComponentBase):
         return index.replace(DATE_PLACEHOLDER, _date_formatted)
 
     @staticmethod
-    def _validate_timezone(tz):
+    def _validate_timezone(tz: str) -> str:
         if tz not in pytz.all_timezones:
             raise UserException(
                 f"Incorrect timezone {tz} provided. Timezone must be a valid DB timezone name. "
@@ -237,17 +183,18 @@ class Component(ComponentBase):
         with open(full_path, "w") as json_file:
             json.dump(results, json_file, indent=4)
 
-    def _create_and_start_ssh_tunnel(self, params) -> None:
-        ssh_params = params.get(KEY_SSH)
-        ssh_username = ssh_params.get(KEY_SSH_USERNAME)
-        keys = ssh_params.get(KEY_SSH_KEYS)
-        private_key = keys.get(KEY_SSH_PRIVATE_KEY)
-        ssh_tunnel_host = ssh_params.get(KEY_SSH_TUNNEL_HOST)
-        ssh_tunnel_port = ssh_params.get(KEY_SSH_TUNNEL_PORT, 22)
-        db_params = params.get(KEY_GROUP_DB)
-        db_hostname = db_params.get(KEY_DB_HOSTNAME)
-        db_port = db_params.get(KEY_DB_PORT)
-        self._create_ssh_tunnel(ssh_username, private_key, ssh_tunnel_host, ssh_tunnel_port, db_hostname, db_port)
+    def _create_and_start_ssh_tunnel(self, config: Configuration) -> None:
+        ssh = config.ssh_options
+        db = config.db
+        private_key = ssh.keys.private_key if ssh.keys else None
+        self._create_ssh_tunnel(
+            ssh_username=ssh.user,
+            private_key=private_key,
+            ssh_tunnel_host=ssh.sshHost,
+            ssh_tunnel_port=ssh.sshPort,
+            db_hostname=db.hostname,
+            db_port=db.port,
+        )
 
         try:
             self.ssh_server.start()
@@ -257,15 +204,24 @@ class Component(ComponentBase):
         logging.info("SSH tunnel is enabled.")
 
     @staticmethod
-    def is_valid_rsa(rsa_key) -> (bool, str):
+    def is_valid_rsa(rsa_key: str) -> tuple[bool, str]:
         if not rsa_key.startswith(RSA_HEADER):
-            return False, f"The RSA key does not start with the correct header: {RSA_HEADER}"
+            return (
+                False,
+                f"The RSA key does not start with the correct header: {RSA_HEADER}",
+            )
         if "\n" not in rsa_key:
             return False, "The RSA key does not contain any newline characters."
         return True, ""
 
     def _create_ssh_tunnel(
-        self, ssh_username, private_key, ssh_tunnel_host, ssh_tunnel_port, db_hostname, db_port
+        self,
+        ssh_username: str,
+        private_key: str,
+        ssh_tunnel_host: str,
+        ssh_tunnel_port: int,
+        db_hostname: str,
+        db_port: int,
     ) -> None:
         is_valid, error_message = self.is_valid_rsa(private_key)
         if is_valid:
@@ -277,11 +233,6 @@ class Component(ComponentBase):
             private_key = get_private_key(private_key, None)
         except SomeSSHException as e:
             raise UserException(e) from e
-
-        try:
-            db_port = int(db_port)
-        except ValueError as e:
-            raise UserException("Remote port must be a valid integer") from e
 
         self.ssh_server = SSHTunnelForwarder(
             ssh_address_or_host=ssh_tunnel_host,
